939ee3b79f932883144e16ea02053d882361b837
priyanshr1972 <priyanshr1972@gmail.com>
2023-03-15 11:18:25 +0530

    first commit

---
+++ Shopping-Cart-master/cookiedemo/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path")
+const app= express()
+const cookieparser = require('cookie-parser');
+
+app.use(cookieparser());
+app.set('views',path.join(__dirname,'views'))
+app.get("/setcookies",(req,res)=>{
+    res.cookie("name","kartik")
+    .cookie("location","delhi")
+    .cookie("language","english")
+    .cookie("age","24")
+    .send("cookie has created")
+})
+
+
+app.get("/seecookies",(req,res)=>{
+    res.send(res.cookies)
+})
+app.listen(4800,()=>{
+    console.log("server running");
+})
